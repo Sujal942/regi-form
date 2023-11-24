9f0dd59aefee81df97ccfd5c965551a0895a11fe
Sujal Jain <63149580+Sujal942@users.noreply.github.com>
2023-11-24 18:40:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# registration-form
+
+1. clone the repo.
+   using :- git clone <link>
+
+2. To install the modules:
+   run :- npm install
+
+3. To start the server:
+   run :- node index.js
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+const Port = process.env.PORT || 3000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.2azck25.mongodb.net/registrationFormDB`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+// register schema
+const registrationSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+// model of registration schema
+const Registration = mongoose.model("Registration", registrationSchema);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await Registration.findOne({ email: email });
+    // cheque for existing user
+    if (!existingUser) {
+      const registrationData = new Registration({
+        name,
+        email,
+        password,
+      });
+      await registrationData.save();
+      res.redirect("/pages/success");
+    } else {
+      console.log("user already exist");
+      res.redirect("/pages/error");
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect("/pages/error");
+  }
+});
+
+app.get("/success", (req, res) => {
+  res.sendFile(__dirname + "/pages/success.html");
+});
+
+app.get("/error", (req, res) => {
+  res.sendFile(__dirname + "/pages/error.html");
+});
+app.listen(Port, () => {
+  console.log(`server is running ${Port}`);
+});
